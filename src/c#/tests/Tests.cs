32faa68959fa470f0558d5fa01102c951f986238
dmccoystephenson <dmccoystephenson@gmail.com>
2023-04-08 16:01:48 -0600

    Added tests for the GameConfig class.

--- src/c#/tests/Tests.cs
+++ src/c#/tests/Tests.cs
@@ -6,6 +6,7 @@ namespace osgtests {
         
         public static void runTests() {
             TestTickCounter.runTests();
+            TestGameConfig.runTests();
         }
     }
 }
---
+++ src/c#/tests/config/TestGameConfig.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+using osg;
+
+namespace osgtests {
+
+    public static class TestGameConfig {
+
+        public static void runTests() {
+            Debug.Log("Running GameConfig tests...");
+            testInstantiation();
+        }
+
+        public static void testInstantiation() {
+            // run
+            GameConfig config = new GameConfig();
+            
+            // check
+            Debug.Assert(config.getChunkSize() == 9);
+            Debug.Assert(config.getLocationScale() == 9);
+            Debug.Assert(config.getUpdateInterval() == 10);
+            Debug.Assert(config.getStatusExpirationTicks() == 500);
+            Debug.Assert(config.getPlayerWalkSpeed() == 20);
+            Debug.Assert(config.getPlayerRunSpeed() == 50);
+        }
+    }    
+}
