4057c5c613c3cb87adef77f132b48cef46393fad
dmccoystephenson <dmccoystephenson@gmail.com>
2023-06-27 22:44:53 -0600

    Displayed duplicate message count when updating status.

--- src/c#/main/ui/Status.cs
+++ src/c#/main/ui/Status.cs
@@ -7,6 +7,7 @@ namespace osg {
         private int ticksToExpire;
         private TextGameObject statusText;
         private int tickLastSet = 0;
+        private int duplicateMessageCount = 0;
 
         public Status(TickCounter tickCounter, int ticksToExpire) {
             this.tickCounter = tickCounter;
@@ -15,6 +16,13 @@ namespace osg {
         }
 
         public void update(string status) {
+            if (getStatus().Contains(status)) {
+                duplicateMessageCount += 1;
+                status += " (x" + (duplicateMessageCount + 1) + ")";
+            }
+            else {
+                duplicateMessageCount = 0;
+            }
             statusText.updateText(status);
             tickLastSet = tickCounter.getTick();
         }
