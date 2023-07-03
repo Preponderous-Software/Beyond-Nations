8c37ef4428db43fa884affde9fb6e7230c2e70de
dmccoystephenson <dmccoystephenson@gmail.com>
2023-07-02 23:20:49 -0600

    Categorized behavior types in `BehaviorType.cs`

--- src/c#/main/behavior/BehaviorType.cs
+++ src/c#/main/behavior/BehaviorType.cs
@@ -2,17 +2,25 @@ namespace osg {
 
     public enum BehaviorType {
         NONE,
+
+        // environment modification
         GATHER_RESOURCES,
-        SELL_RESOURCES,
+        PLANT_SAPLING,
+
+        // movement
         WANDER,
-        PURCHASE_FOOD,
-        CONSTRUCT_SETTLEMENT,
         GO_TO_HOME_SETTLEMENT,
         EXIT_SETTLEMENT,
-        PLANT_SAPLING,
-        CONSTRUCT_STALL,
+
+        // trading
+        SELL_RESOURCES, // TODO: reimplement
+        PURCHASE_FOOD, // TODO: reimplement
         PURCHASE_STALL, // TODO: implement
         TRANSFER_ITEMS_TO_STALL, // TODO: implement
         COLLECT_PROFIT_FROM_STALL, // TODO: implement
+
+        // construction
+        CONSTRUCT_SETTLEMENT,        
+        CONSTRUCT_STALL,
     }
 }
